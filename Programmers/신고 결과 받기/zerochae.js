a2a8b5bb4ce51c5302aa21317dcffc35a832fa55
zerochae <zerochae@kakao.com>
2022-02-01 18:52:05 +0900

    Add: git ignore 설정

---
+++ Programmers/신고 결과 받기/zerochae.js
@@ -0,0 +1,21 @@
+let id_list = ["muzi", "frodo", "apeach", "neo"];
+let report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+let k = 2;
+
+function solution(id_list, report, k) {
+  var answer = [];
+
+  console.log(new member(id_list[0],0))
+
+  
+  return answer;
+}
+
+class member{
+  constructor(nm,count){
+    this.nm = nm;
+    this.count = count;
+  }
+}
+
+solution(id_list,report,k);
