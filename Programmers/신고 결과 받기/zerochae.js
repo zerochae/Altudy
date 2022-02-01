a1161694f93241648594c3a118c7adb2e700347c
zerochae <zerochae@kakao.com>
2022-02-02 00:40:42 +0900

    Add: 신고 결과 받기

--- Programmers/신고 결과 받기/zerochae.js
+++ Programmers/신고 결과 받기/zerochae.js
@@ -1,21 +1,71 @@
 let id_list = ["muzi", "frodo", "apeach", "neo"];
-let report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+let report = [
+  "muzi frodo",
+  "apeach frodo",
+  "frodo neo",
+  "muzi neo",
+  "apeach muzi",
+];
 let k = 2;
 
+solution(id_list, report, k);
+
 function solution(id_list, report, k) {
-  var answer = [];
 
-  console.log(new member(id_list[1],0))
+  class member {
+    constructor(nm, mail, count, report) {
+      this.nm = nm;
+      this.report = report;
+      this.mail = mail;
+      this.count = count;
+    }
+  }
 
-  
+  let answer = [];
+  let memberList = []; // 멤버들 정보
+
+  for (let mem of id_list) {
+    memberList.push(new member(mem, 0, 0, [])); // 멤버 기본 정보 저장
+  }
+
+  let set = new Set();
+
+  for (let rep of report) {
+    set.add(rep);
+  }
+
+  for (let item of set) {
+    let reporter = item.split(" ")[0];
+    let reported = item.split(" ")[1];
+
+    memberList.map((mem) => {
+      if (mem.nm === reporter) return mem.report.push(reported);
+    });
+  }
+
+  for (mem of memberList) {
+    for (rep of mem.report) {
+      memberList.map((member) => {
+        if (member.nm === rep) {
+          member.count++;
+        }
+      });
+    }
+  }
+
+  for (let mem of memberList) {
+    if (mem.count >= k) {
+      memberList.map((member) => {
+        if (member.report.includes(mem.nm)) member.mail++;
+      });
+    }
+  }
+
+  for (let mem of memberList) {
+    answer.push(mem.mail);
+  }
+
+  console.log(answer);
+
   return answer;
 }
-
-class member{
-  constructor(nm,count){
-    this.nm = nm;
-    this.count = count;
-  }
-}
-
-solution(id_list,report,k);
