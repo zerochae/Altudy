398ca03203db7bf1b79141dafe6bc383113b908d
zerochae <zerochae@kakao.com>
2022-02-02 15:31:46 +0900

    Modify: 기능 개발

--- Programmers/기능개발/zerochae.js
+++ Programmers/기능개발/zerochae.js
@@ -5,6 +5,52 @@ const speeds = [1,30,5];
 solution(progresses,speeds);
 
 function solution(progresses, speeds) {
-  let answer = [];
+
+  const answer = [];
+  speeds = speeds.reverse();
+  progresses = progresses.reverse();
+
+  class Stack {
+    constructor() {
+      this.arr = [];
+      this.length = 0;
+    }
+    push(item) {
+      this.arr[this.length++] = item;
+    }
+    pop() {
+      return this.length > 0 ? this.arr[--this.length] : null;
+    }
+    isEmpty() {
+      return this.length === 0
+    }
+    top() {
+      return this.arr[this.length - 1];
+    }
+  }
+
+  const stack = new Stack();
+
+  for(let item of progresses){
+    stack.push(item);
+  }
+
+  while(!stack.isEmpty()){
+
+    let count = 0;
+
+    for(let index in stack.arr){
+      stack.arr[index] += speeds[index];
+    }
+
+    while(stack.top() >= 100){
+      stack.pop();
+      count++;
+    }
+    if(count !== 0) answer.push(count);
+  }
+
+  console.log(answer);
+
   return answer;
 }
