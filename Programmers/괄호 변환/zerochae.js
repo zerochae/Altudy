80a26c97b0b8dd4a946c700d6d76305cd94af729
zerochae <zerochae@kakao.com>
2022-02-03 23:48:46 +0900

    Modify: 괄호 변환

--- Programmers/괄호 변환/zerochae.js
+++ Programmers/괄호 변환/zerochae.js
@@ -8,13 +8,9 @@ function solution(p) {
 
 function recur(str) {
   if (isCorrect(str)) return str;
-
   if (str.length === 0) return "";
-
-  let { u, v } = isBalance(str);
-
+  let { u, v } = isBalance(str); 
   if (isCorrect(u)) return u + recur(v);
-
   let result = "(" + recur(v) + ")";
   for (str of u.substring(1, u.length - 1).split("")) {
     result += str === "(" ? ")" : "(";
@@ -23,35 +19,28 @@ function recur(str) {
 }
 
 function isCorrect(str) {
+
   let items = str.split("");
   let stack = [];
 
   items.map((item) => {
     switch (item) {
-      case "(":
-        stack.push(item);
-        break;
-      case ")":
-        if (stack.length !== 0) stack.pop();
-        else {
-          return false;
-        }
-        break;
+      case "(": stack.push(item); break;
+      case ")": if (stack.length !== 0) stack.pop(); else return false;
+      break;
     }
   });
   return stack.length === 0;
 }
 
 function isBalance(str) {
-  let items = str.split("");
 
-  let open = 0;
-  let close = 0;
-  let u = "";
-  let v = "";
+  let items = str.split("");
+  let open = 0; let close = 0;
+  let u = ""; let v = "";
 
   for (let item of items) {
-    u += item;
+    u += item; 
     isWhat(item) ? open++ : close++;
     if (open !== 0 && close !== 0 && open === close) break;
   }
@@ -66,13 +55,7 @@ function isBalance(str) {
 
 function isWhat(str) {
   switch (str) {
-    case "(":
-      return true;
-    case ")":
-      return false;
+    case "(": return true;
+    case ")": return false;
   }
 }
-
-let result = solution(p);
-
-console.log(result);
