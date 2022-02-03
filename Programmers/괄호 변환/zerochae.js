6716b243ab5d3d2113fb3b62fdc910e99406bbda
zerochae <zerochae@kakao.com>
2022-02-04 00:02:23 +0900

    Modify: 괄호 변환

--- Programmers/괄호 변환/zerochae.js
+++ Programmers/괄호 변환/zerochae.js
@@ -9,12 +9,15 @@ function solution(p) {
 function recur(str) {
   if (isCorrect(str)) return str;
   if (str.length === 0) return "";
+
   let { u, v } = isBalance(str); 
+
   if (isCorrect(u)) return u + recur(v);
+
   let result = "(" + recur(v) + ")";
-  for (str of u.substring(1, u.length - 1).split("")) {
-    result += str === "(" ? ")" : "(";
-  }
+
+  for (str of u.substring(1, u.length - 1).split("")) result += str === "(" ? ")" : "(";
+  
   return result;
 }
 
@@ -27,9 +30,9 @@ function isCorrect(str) {
     switch (item) {
       case "(": stack.push(item); break;
       case ")": if (stack.length !== 0) stack.pop(); else return false;
-      break;
     }
   });
+
   return stack.length === 0;
 }
 
@@ -44,8 +47,7 @@ function isBalance(str) {
     item === "(" ? open++ : close++;
     if (open !== 0 && close !== 0 && open === close) break;
   }
-
   v = str.substring(u.length);
-
+  
   return { u, v };
 }
