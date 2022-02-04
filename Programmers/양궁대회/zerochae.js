064553c773726fdcebc728cd53f981ac3a079231
zerochae <zerochae@kakao.com>
2022-02-04 22:22:53 +0900

    Modify: 양궁대회

--- Programmers/양궁대회/zerochae.js
+++ Programmers/양궁대회/zerochae.js
@@ -10,34 +10,36 @@ const solution = (n, score_apeach) => {
 };
 
 const dfs = (depth, score_apeach, score_lion, shot) => {
-
-  if(shot < 0) return;
+  if (shot < 0) return;
 
   if (depth === 10) {
+    let apeach = 0;
+    let lion = 0;
+    score_lion[10] = shot;
 
-    let apeach = 0; let lion = 0; score_lion[10] = shot;
-    
     for (let i in score_lion)
-    if (score_apeach[i] !== 0 || score_lion[i] !== 0)
-    score_lion[i] > score_apeach[i] ? (lion += 10 - i) : (apeach += 10 - i);
-    
+      if (score_apeach[i] !== 0 || score_lion[i] !== 0)
+        score_lion[i] > score_apeach[i] ? (lion += 10 - i) : (apeach += 10 - i);
+
     if (lion - apeach > max) {
       result = [...score_lion];
       max = lion - apeach;
-    } else if (lion - apeach === max) result = [...compare(result,score_lion)]
-    
+    } else if (lion - apeach === max) result = [...compare(result, score_lion)];
+
     return;
   }
-    score_lion[depth] = score_apeach[depth] + 1; dfs(depth + 1, score_apeach, score_lion, shot - score_lion[depth]);
-    score_lion[depth] = 0; dfs(depth + 1, score_apeach, score_lion, shot);
+  score_lion[depth] = score_apeach[depth] + 1;
+  dfs(depth + 1, score_apeach, score_lion, shot - score_lion[depth]);
+  score_lion[depth] = 0;
+  dfs(depth + 1, score_apeach, score_lion, shot);
 };
 
-const compare = (arr1 , arr2) => {
+const compare = (arr1, arr2) => {
+  arr1 = arr1.reverse();
+  arr2 = arr2.reverse();
 
-  arr1 = arr1.reverse(); arr2 = arr2.reverse();
-
-  for(let index in arr1){
-    if(arr1[index] > arr2[index]) return arr1.reverse();
-    else if(arr1[index] < arr2[index]) return arr2.reverse();
+  for (let index in arr1) {
+    if (arr1[index] > arr2[index]) return arr1.reverse();
+    else if (arr1[index] < arr2[index]) return arr2.reverse();
   }
-}
+};
