443b3c5da332c2a0c0f268e9b256ec736be0924f
zerochae <zerochae@kakao.com>
2022-02-06 19:26:28 +0900

    Add: 124나라의 숫자

---
+++ Programmers/124나라의숫자/zerochae.js
@@ -0,0 +1,23 @@
+/* get input */
+let n = 11100;
+/* get input end*/
+
+/* set param */
+let result = "";
+/* set param end*/
+
+/* solve */
+const solution = (n) => {
+  while (n > 0) {
+    result += n % 3 === 0 ? n / Math.floor(n / 3) : n % 3; /*   n / Math.floor(n / 3) === 3   */ 
+    n = n % 3 === 0 ? Math.floor(n / 3) - 1 : Math.floor(n / 3);
+  }
+
+  return result.split("").reverse().join("").replace(/3/gi, "4");
+};
+/* solve end*/
+
+/* print output */
+const output = solution(n);
+console.log(output);
+/* print output end*/
