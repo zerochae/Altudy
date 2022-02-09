32ca3b91472b11a2c73cc3f7075a568cc6916ada
zerochae <zerochae@kakao.com>
2022-02-09 19:22:19 +0900

    Add: 거리두기 확인하기

---
+++ Programmers/거리두기 확인하기/zerochae.js
@@ -0,0 +1,39 @@
+/* get input */
+let n = [
+  ["POOOP", "OXXOX", "OPXPX", "OOXOX", "POXXP"],
+  ["POOPX", "OXPXP", "PXXXO", "OXXXO", "OOOPP"],
+  ["PXOPX", "OXOXP", "OXPOX", "OXXOP", "PXPOX"],
+  ["OOOXX", "XOOOX", "OOOXX", "OXOOX", "OOOOO"],
+  ["PXPXP", "XPXPX", "PXPXP", "XPXPX", "PXPXP"],
+];
+/* get input end*/
+
+/* solve */
+const solution = (rooms) => {
+
+  return rooms.map((room) => // room[0] room[1] ... room[4]
+    room.some((row, ri) => // "POOOP" ... "POXXP"
+      row.split("").some((col, ci, arr) => { // "P" "O" "O" "O" "P" ... "X" "X" "P"
+        return col === "X" ? false : search(room, arr, ri, ci, col); })
+  ) ? 0 : 1 );
+};
+
+const search = (room, arr, ri, ci, col) => {
+
+  const location = [ 
+    (room[ri - 1] || "").charAt(ci), // up undefined?
+    (room[ri + 1] || "").charAt(ci), // down undefined?
+    arr[ci - 1], // left
+    arr[ci + 1] ]; // right 
+
+  const count = new Array(4).fill(null).map( (row,index) => {
+    return location[index] }).filter((el) => el === "P").length; // P count > 0 , O count < 1 
+
+  return (col === "P" && count > 0) || (col === "O" && count > 1);
+};
+/* solve end*/
+
+/* print output */
+const output = solution(n);
+console.log(output);
+/* print output end*/
