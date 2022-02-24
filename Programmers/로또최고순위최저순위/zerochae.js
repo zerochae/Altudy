c9f605aac40c1bb6ea6e0ccc5c710e38c092ef31
zerochae <zerochae@kakao.com>
2022-02-24 18:45:59 +0900

    Modify: 로또의 최고 순위와 최저 순위

--- Programmers/로또최고순위최저순위/zerochae.js
+++ Programmers/로또최고순위최저순위/zerochae.js
@@ -1,15 +1,7 @@
 /* get input */
-// const lottos = [0, 0, 0, 0, 0, 0];
-// const win_nums = [38, 19, 20, 40, 15, 25];
 const lottos = [44, 1, 0, 0, 31, 25];
 const win_nums = [31, 10, 45, 1, 6, 19];
-// const lottos = [45, 4, 35, 20, 3, 9];
-// const win_nums = [45, 4, 35, 20, 3, 9];
 /* get input end*/
-
-/* set param */
-
-/* set param end*/
 
 /* solve */
 const solution = (lottos, win_nums) => {
