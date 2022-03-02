5125bf6a9fc47920a627f0d0f891f7ec324f3eeb
zerochae <zerochae@kakao.com>
2022-03-02 14:13:57 +0900

    Modify: 키패드 누르기

--- Programmers/키패드 누르기/zerochae.js
+++ Programmers/키패드 누르기/zerochae.js
@@ -1,5 +1,5 @@
 /* get input */
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+const numbers = [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5];
 const hand = "right";
 /* get input end */
 
@@ -19,48 +19,54 @@ const lastPad = {
 /* solve */
 const solution = (numbers, hand) => {
   return numbers.reduce((acc, num) => {
-    let pad;
     const target = getPosition(num);
-    const left = getPosition(lastPad.left);
-    const right = getPosition(lastPad.right);
+
     if (target[1] === 0) {
-      pad = "L";
       lastPad.left = num;
-    } else if (target[1] === 2) {
-      pad = "R";
-      lastPad.right = num;
-    } else {
-      const distance_left = Math.ceil(
-        Math.sqrt(
-          Math.abs((target[0] - left[0]) ** 2) +
-            Math.abs((target[1] - left[1]) ** 2)
-        )
-      );
-      const distance_right = Math.ceil(
-        Math.sqrt(
-          Math.abs((target[0] - right[0]) ** 2) +
-            Math.abs((target[1] - right[1]) ** 2)
-        )
-      );
-      if (distance_left === distance_right) {
-        if (hand === "left") {
-          pad = "L";
-          lastPad.left = num;
-        } else {
-          pad = "R";
-          lastPad.right = num;
-        }
-      } else {
-        if (distance_left < distance_right) {
-          pad = "L";
-          lastPad.left = num;
-        } else {
-          pad = "R";
-          lastPad.right = num;
-        }
-      }
+      return acc + "L";
     }
-    return acc + pad;
+
+    if (target[1] === 2) {
+      lastPad.right = num;
+      return acc + "R";
+    }
+
+    const left = getPosition(lastPad.left);
+    const distance_left = Math.ceil(
+      Math.sqrt(
+        Math.abs((target[0] - left[0]) ** 2) +
+          Math.abs((target[1] - left[1]) ** 2)
+      )
+    );
+
+    const right = getPosition(lastPad.right);
+    const distance_right = Math.ceil(
+      Math.sqrt(
+        Math.abs((target[0] - right[0]) ** 2) +
+          Math.abs((target[1] - right[1]) ** 2)
+      )
+    );
+
+    if (distance_left === distance_right && hand === "left") {
+      lastPad.left = num;
+      return acc + "L";
+    }
+
+    if (distance_left === distance_right && hand === "right") {
+      lastPad.right = num;
+      return acc + "R";
+    }
+
+    if (distance_left < distance_right) {
+      lastPad.left = num;
+      return acc + "L";
+    }
+
+    if (distance_left > distance_right) {
+      lastPad.right = num;
+      return acc + "R";
+    }
+
   }, "");
 };
 
