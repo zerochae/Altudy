530d8988d30350bffcd0c45d58291afe91ae7dd5
zerochae <zerochae@kakao.com>
2022-03-12 17:32:23 +0900

    Add: 15650_N과M 2

---
+++ BOJ/15650_N과M 2/zerochae.js
@@ -0,0 +1,41 @@
+/* get input */
+const fs = require("fs");
+const input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin").toString()
+    : `4 2`;
+/* get input end */
+
+/* solve */
+const [n, m] = input.split(" ").map(Number);
+const arr = new Array(m);
+const visit = new Array(n + 1).fill(false);
+const result = [];
+const solution = () => {
+  dfs(0);
+  return result.join("");
+};
+const dfs = (depth) => {
+  if (depth === m) {
+    arr.forEach((el) => {
+      result.push(el + " ");
+    });
+    result.push("\n");
+    return;
+  }
+
+  for (let i = 1; i <= n; i++) {
+    if (!visit[i]) {
+      visit[i] = true;
+      arr[depth] = i;
+      dfs(depth + 1);
+      for (let j = i + 1; j <= n; j++) visit[j] = false;
+    }
+  }
+};
+/* solve end */
+
+/* print output */
+const output = solution();
+console.log(output);
+/* print output end */
