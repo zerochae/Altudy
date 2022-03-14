783677b69ecc0aeb91170ea973954cfa101b8168
zerochae <zerochae@kakao.com>
2022-03-14 17:41:37 +0900

    Add: n^2배열 자르기

---
+++ Programmers/n^2배열 자르기/zerochae.js
@@ -0,0 +1,22 @@
+const n = 3;
+const left = 2;
+const right = 5;
+
+const solution = (n, left, right) => {
+  return new Array(right - left + 1).fill(null).reduce(
+    (result) => {
+      result.arr.push(
+        Math.max(Math.floor(result.index / n) + 1, (result.index++ % n) + 1)
+      );
+      return result;
+    },
+    {
+      arr: [],
+      index: left,
+    }
+  ).arr;
+};
+
+const output = solution(n, left, right);
+
+console.log(output);
