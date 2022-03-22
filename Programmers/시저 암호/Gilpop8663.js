8af783707223da69e7dc4dbddd5606fd502d7a9d
gilpop8663 <wolfye@naver.com>
2022-03-23 00:43:04 +0900

    Modify: 시저 암호

--- Programmers/시저 암호/Gilpop8663.js
+++ Programmers/시저 암호/Gilpop8663.js
@@ -1,24 +1,35 @@
 /* get input */
-const s="AB";
-let n=1;
+const s = "AB";
+let n = 1;
 /* get input end */
 
 /* solve */
 function solution(s, n) {
-    var answer = '';
-
-    for(let i=0;i<s.length;i++){
-        if(s[i] ===" ") answer +=" "
-        else{
-            let strNum = s[i].charCodeAt() + n
-            answer += String.fromCharCode(strNum)
-
+  var answer = "";
+  for (let i = 0; i < s.length; i++) {
+    let strNum = 0;
+    if (s[i] === " ") answer += " ";
+    else {
+      if (s[i].charCodeAt() >= 65 && s[i].charCodeAt() <= 90) {
+        if (s[i].charCodeAt() + n > 90) {
+          strNum = s[i].charCodeAt() + n - 26;
+        } else {
+          strNum = s[i].charCodeAt() + n;
         }
+      } else {
+        if (s[i].charCodeAt() + n > 122) {
+          strNum = s[i].charCodeAt() + n - 26;
+        } else {
+          strNum = s[i].charCodeAt() + n;
+        }
+      }
+      answer += String.fromCharCode(strNum);
     }
-    return answer;
+  }
+  return answer;
 }
 /* solve end */
 
 /* print output */
-console.log(solution(s, n))
+console.log(solution(s, n));
 /* print output end */
