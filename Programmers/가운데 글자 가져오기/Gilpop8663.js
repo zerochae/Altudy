a270c1c3901c24766b84c22c58c9d7e245ac50d8
gilpop8663 <wolfye@naver.com>
2022-03-28 13:57:17 +0900

    Add: 튜플

--- Programmers/가운데 글자 가져오기/Gilpop8663.js
+++ Programmers/가운데 글자 가져오기/Gilpop8663.js
@@ -1,12 +1,18 @@
-const s="abcde";
+/* get input */
+const s = "abcde";
+/* get input end */
 
+/* solve */
 function solution(s) {
-    var answer = '';
-    if(s.length %2 === 1){
-        return answer = s[Math.floor(s.length/2)]
-    }else{
-        return answer =s[s.length/2 -1]+s[s.length/2]
-    }
+  var answer = "";
+  if (s.length % 2 === 1) {
+    return (answer = s[Math.floor(s.length / 2)]);
+  } else {
+    return (answer = s[s.length / 2 - 1] + s[s.length / 2]);
+  }
 }
+/* solve end */
 
-console.log(solution(s))
+/* print output */
+console.log(solution(s));
+/* print output end */
