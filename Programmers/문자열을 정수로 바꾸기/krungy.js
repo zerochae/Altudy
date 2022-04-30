d5c1d50ab14ae8be20470f01c701afe374dd69ba
krungy <furler711@gmail.com>
2022-04-30 13:58:47 +0900

    Add: 문자열을 정수로 바꾸기

---
+++ Programmers/문자열을 정수로 바꾸기/krungy.js
@@ -0,0 +1,16 @@
+/* get input */
+const s = "1234"; // output = 1234
+// const s = "-1234"; // output = -1234
+/* get input end */
+
+/* solve */
+function solution(s) {
+  var answer = s * 1;
+  return answer;
+}
+/* solve end */
+
+/* print output */
+const output = solution(s);
+console.log(output);
+/* print output end */
