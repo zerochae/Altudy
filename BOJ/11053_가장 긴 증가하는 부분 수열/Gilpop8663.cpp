453c4c8b6cd3f7d2df5d1b5f803a4c68aab42337
gilpop8663 <wolfye@naver.com>
2022-05-12 06:27:16 +0900

    Add: 11053_가장 긴 증가하는 부분 수열

---
+++ BOJ/11053_가장 긴 증가하는 부분 수열/Gilpop8663.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int arr[1005];
+
+int d[1005];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        d[i] = 1;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[j] < arr[i])
+            {
+                d[i] = max(d[i], d[j] + 1);
+            }
+        }
+    }
+
+    cout << *max_element(d, d + n);
+}
