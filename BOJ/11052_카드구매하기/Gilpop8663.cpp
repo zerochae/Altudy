a459e53d8ac82d2d8bda2980fcf0c04fd5f2ce5e
gilpop8663 <wolfye@naver.com>
2022-05-15 15:14:54 +0900

    Add: 11052_카드 구매하기

---
+++ BOJ/11052_카드구매하기/Gilpop8663.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int d[1005];
+
+int arr[1005];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // d[1] = arr[1];
+    // d[2] = arr[1] * 2 || d[1] + arr[1] || arr[2];
+    // d[3] = arr[1] * 3 || d[2] + arr[1] || arr[3];
+    // d[4] = arr[1] * 4 || d[3] + arr[3] || arr[4];
+
+    // d[1] = 1;
+    // d[2] = 5;
+    // d[3] = 6;
+    // d[4] = 10;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            d[i] = max(d[i], d[i - j] + arr[j]);
+        }
+    }
+    cout << d[n];
+}
