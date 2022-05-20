2622702d2d9fe866dfeedcc93b9e387972891f63
gilpop8663 <wolfye@naver.com>
2022-05-20 11:23:16 +0900

    Add: 10816_숫자 카드 2

---
+++ BOJ/10816_숫자카드2/Gilpop8663.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+int arr[500005];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr, arr + n);
+
+    cin >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int num;
+        cin >> num;
+        // cout << upper_bound(arr, arr + n, num) << " " << lower_bound(arr, arr + n, num) << "\n";
+        cout << upper_bound(arr, arr + n, num) - lower_bound(arr, arr + n, num) << " ";
+    }
+}
