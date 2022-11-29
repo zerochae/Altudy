3516edc45f251c9f89ae1539f4308c84deb60229
gilpop8663 <wolfye@naver.com>
2022-11-29 23:57:05 +0900

    Add:15894_수학은 체육과목 입니다

---
+++ BOJ/15894_수학은 체육과목 입니다/Gilpop8663.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+
+    long long ans = n * 4;
+    cout << ans;
+}
