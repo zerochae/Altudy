15e5d25f7bc9bb4fbc61c9ec047089d40fe7177f
김영길 <gim-yeong-gil@gim-yeong-gil-ui-MacBookAir.local>
2022-12-16 02:38:36 +0900

    Add:15680_연세대학교

---
+++ BOJ/15680_연세대학교/Gilpop8663.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+string arr[2]={"YONSEI","Leading the Way to the Future"};
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin>>n;
+
+    cout<<arr[n];
+    return 0;
+}
